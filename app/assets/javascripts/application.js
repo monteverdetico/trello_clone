428089116b886f1330ab5196363733521f6b6679
monteverdetico <marcelo.guindon@gmail.com>
2013-09-02 16:27:09 -0700

    don't initialize for heroku; remove public index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require underscore
+//= require backbone
+//= require trello_clone
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
 //= require_tree .
--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module TrelloClone
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Don't initialize for Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
