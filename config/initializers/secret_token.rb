1f2a86f2e1beea9443e67ec56d7578751896a050
monteverdetico <marcelo.guindon@gmail.com>
2013-09-02 14:36:20 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrelloClone::Application.config.secret_token = '451ab7ceab86287c8ee9d62e9c9baaf359a0afdcfcb259a2470eb6523bae15c319a33482e5474bf1a9d58bd3c5c42e24768661be8821bea53b05a53fc7e10c18'
